9835732843945a4a636949467ba6d72627572c48
pjones89 <pjones89@byui.edu>
2021-05-22 01:01:20 -0600

    commit

---
+++ lesson5/js/weather.js
@@ -0,0 +1,8 @@
+function myMenu() {
+    var x = document.getElementById("menuLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else{
+        x.style.display = "block";
+    }
+}
