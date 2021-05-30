5c84f4ca24837960f4b0a378cc9b50608f610c5d
pjones89 <pjones89@byui.edu>
2021-05-29 22:09:25 -0600

    commit

--- lesson6/js/weather.js
+++ lesson6/js/weather.js
@@ -10,3 +10,13 @@ function myMenu() {
 // the following code has been taken (and somewhat modified) from the codepen provided in week 4: "codepen.io/blazzard-jason/pen/gmGxaj"
 var x = document.getElementById("menulinks");
 window.onresize = () => {if (window.innerWidth >= 40em) {x.style.display = "inline";} else{x.style.display = "none"}}
+
+
+//taken from our lesson this week
+WebFont.load({
+    google: {
+      families: [
+         'fontfamilynameofyourchoice'
+      ]
+    }
+  });
