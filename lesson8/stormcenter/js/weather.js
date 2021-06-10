6c6b227e18238d15ad4a767529d897300344750a
pjones89 <pjones89@byui.edu>
2021-06-10 07:25:16 -0600

    commit

--- lesson8/stormcenter/js/weather.js
+++ lesson8/stormcenter/js/weather.js
@@ -3,18 +3,24 @@
     document.getElementById("rangevalue").innerHTML = range;
 }
 
-//function myMenu() {
-    //var x = document.getElementById("menulinks");
-    //if (x.style.display === "inline") {
-        //x.style.display = "none";
-    //} else{
-        //x.style.display = "inline";
-    //}
-//}
+function myMenu() {
+    var x = document.getElementById("menulinks");
+    if (x.style.display === "inline") {
+        x.style.display = "none";
+    } else{
+        x.style.display = "inline";
+    }
+}
 
 // the following code has been taken (and somewhat modified) from the codepen provided in week 4: "codepen.io/blazzard-jason/pen/gmGxaj"
 var x = document.getElementById("menulinks");
-window.onresize = () => {if (window.innerWidth >= 40em) {x.style.display = "inline";} else{x.style.display = "none"}}
+window.onresize = () => {
+    if (window.innerWidth >= 40) {
+        x.style.display = "inline";
+    } else {
+        x.style.display = "none"
+    }
+}
 
 
 //taken from our lesson this week
@@ -26,7 +32,3 @@ WebFont.load({
     }
   });
 
-  //taken from codepen.io/blazzard-jason/pen/oMRdJg (codepen that was included in the assignment for week 8)
-  function changeRange(range) {
-      document.getElementById("rangevalue").innerHTML = range;
-  }
